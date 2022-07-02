70dd5baa07a0865a9775772b4057dc0aaf6254b0
Jack Steiner <steiner.jack01@gmail.com>
2022-07-02 16:19:38 -0500

    added first function and started adding albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,5 +2,43 @@ console.log('***** Music Collection *****')
 
 collection = []
 
-function addToCollection(title, artist, yearPublished ) {
+const albumOne = {
+    title: 'Virga I',
+    artist: 'Eluvium',
+    yearPublished: 2020,
+}
+
+function addToCollection(title, artist, yearPublished) {
+    const newAlbum = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    }
+    collection.push(newAlbum);
 };
+
+addToCollection('Virga I', 'Eluvium', 2020);
+console.log('Collection', collection);
+
+const albumTwo = {
+    title: 'Forever',
+    artist: 'Joy Again',
+    yearPublished: 2014,
+}
+
+const albumThree = {
+    title: 'Dummy',
+    artist: 'Portishead',
+    yearPublished: 1990,
+}
+
+const albumFour = {
+    title: 'Graduation',
+    artist: 'Kanye West',
+    yearPublished: 2007,
+}
+const albumFive = {
+    title: 'Public Strain',
+    artist: 'Women',
+    yearPublished: 2010,
+}
